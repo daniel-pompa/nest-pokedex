7bd130cd32be4e700afa5cc1aaea7aba4af73e6a
Daniel <daniel.40.pompa@gmail.com>
2026-03-05 00:36:27 +0100

    feat(seed): implement bulk insert and database cleanup

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,28 +1,40 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
 import { PokeAPIResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async executeSeed() {
-    try {
-      const { data } = await firstValueFrom(
-        this.httpService.get<PokeAPIResponse>('pokemon?limit=10'),
-      );
+    // 1. We clean the database
+    await this.pokemonModel.deleteMany({});
 
-      const pokemonToInsert = data.results.map(({ name, url }) => {
-        const pokemonNumber = +url.split('/').at(-2)!;
-        return { name, url, pokemonNumber };
-      });
+    // 2. API request limited to 100
+    const { data } = await firstValueFrom(
+      this.httpService.get<PokeAPIResponse>('pokemon?limit=100'),
+    );
 
-      return pokemonToInsert;
-    } catch {
-      throw new InternalServerErrorException(
-        'Error fetching data from PokéAPI',
-      );
-    }
+    // 3. Data transformation
+    const pokemonToInsert = data.results.map(({ name, url }) => {
+      const pokemonNumber = +url.split('/').at(-2)!;
+      return { name: name.toLowerCase(), pokemonNumber };
+    });
+
+    // 4. Insertion of the new 100 records
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return {
+      message: 'Seed executed successfully',
+      count: pokemonToInsert.length,
+    };
   }
 }
