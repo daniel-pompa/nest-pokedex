4c61c09c040719f030796cfada2a8c653f937581
Daniel <daniel.40.pompa@gmail.com>
2026-03-05 00:35:41 +0100

    feat(seed): import pokemon module into seed module

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 @Module({
-  imports: [HttpModule.register({ baseURL: 'https://pokeapi.co/api/v2/' })],
+  imports: [
+    HttpModule.register({ baseURL: 'https://pokeapi.co/api/v2/' }),
+    PokemonModule,
+  ],
   controllers: [SeedController],
   providers: [SeedService],
 })
