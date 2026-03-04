84db510d3842d3dc1da9fd9b75fb0e0cd6a639a2
Daniel <daniel.40.pompa@gmail.com>
2026-03-04 22:13:43 +0100

    feat(app): register common module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,25 +4,24 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-
     MongooseModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
       }),
     }),
-
     PokemonModule,
+    CommonModule,
   ],
 })
 export class AppModule {}
