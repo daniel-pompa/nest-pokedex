0a697bedaf76504587725fae22167cc7f8f31701
Daniel <daniel.40.pompa@gmail.com>
2026-03-05 23:43:34 +0100

    feat(common): export HTTP_ADAPTER token

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { AxiosAdapter } from './adapters/axios.adapter';
 
-@Module({})
+@Module({
+  imports: [HttpModule], // Required to provide HttpService to AxiosAdapter
+  providers: [
+    {
+      provide: 'HTTP_ADAPTER',
+      useClass: AxiosAdapter,
+    },
+  ],
+  exports: ['HTTP_ADAPTER'],
+})
 export class CommonModule {}
